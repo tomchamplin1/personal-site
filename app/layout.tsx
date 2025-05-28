344b2c6f8bf51fe8b15f067d519031e2f4ee6e8e
Tom Champlin <64120886+tomchamplin1@users.noreply.github.com>
2025-05-28 11:56:20 -0500

    Migrate to app router

---
+++ app/layout.tsx
@@ -0,0 +1,59 @@
+import Navbar from "@/components/Navbar";
+import { Inter } from "next/font/google";
+import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
+
+import { ThemeProvider } from "@/components/theme-provider";
+
+function cn(...inputs) {
+  return twMerge(clsx(inputs));
+}
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Tom Champlin",
+  description: "Tom Champlin portfolio",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="shortcut icon" href="/favicon_io/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon_io/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon_io/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon_io/favicon-16x16.png"
+        />
+        <title>Tom Champlin</title>
+        <meta name="description" content="Tom Champlin portfolio" />
+      </head>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.className
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <div className="relative flex min-h-screen flex-col">
+            {/* <Navbar /> */}
+            <div className="flex-1">{children}</div>
+          </div>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,21 @@
+"use client";
+
+import React from "react";
+import "./index.css";
+import HomeAbout from "@/components/HomeAbout";
+import Experience from "@/components/Experience";
+import Projects from "@/components/Projects";
+
+export default async function Index() {
+  return (
+    <div className="bg-black min-h-screen flex flex-col">
+      <div className="App">
+        <div className="bg-black min-h-screen ">
+          <HomeAbout />
+          <Experience />
+          <Projects />
+        </div>
+      </div>
+    </div>
+  );
+}
